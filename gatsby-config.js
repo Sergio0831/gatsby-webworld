18ab142ec013d64389d786301ca4ce72f41e8b31
Sergio0831 <sergejs.ivcenko@gmail.com>
2021-08-19 05:39:51 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,28 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-remark`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: "Web World",
+    description: "wed dev portfolio",
+    copyright: "This website is copyright Web World",
+    contact: "me@testmail.com",
+  },
 }
---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+          dignissimos quasi doloribus cum ipsum et ullam dicta voluptates amet
+          eligendi.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+          dignissimos quasi doloribus cum ipsum et ullam dicta voluptates amet
+          eligendi.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+          dignissimos quasi doloribus cum ipsum et ullam dicta voluptates amet
+          eligendi.
+        </p>
+      </div>
+    </Layout>
+  )
+}
